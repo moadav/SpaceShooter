b463f4ba2fd4736eec39829e6ac891b28fe0933d
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2021-07-18 01:01:12 +0200

    adding files

---
+++ SpaceShooter.MyApi/Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SpaceShooter.AdataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceShooter.MyApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            var sqlConnBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = "Donau.hiof.no",
+                InitialCatalog = "mohammed",
+                UserID = "mohammed",
+                Password = "y6DNU=7;"
+            };
+
+            services.AddDbContext<UserLoginContext>(options => options.UseSqlServer(sqlConnBuilder.ConnectionString));
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
