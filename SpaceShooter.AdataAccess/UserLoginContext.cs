0cd2f8da92fa4c97072874072347e242ccd85705
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2021-09-25 23:00:52 +0200

    Add files via upload

---
+++ SpaceShooter.AdataAccess/UserLoginContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaceShooter.LoginModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceShooter.AdataAccess
+{
+    /// <summary>
+    /// the UserLoginContext
+    /// </summary>
+    /// <seealso cref="Microsoft.EntityFrameworkCore.DbContext" />
+    public class UserLoginContext : DbContext
+    {
+
+
+        public UserLoginContext()
+        {
+
+        }
+        /// <summary>
+        /// Gets or sets the users.
+        /// </summary>
+        /// <value>
+        /// The users.
+        /// </value>
+        public DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserLoginContext"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        public UserLoginContext(DbContextOptions<UserLoginContext> options) : base(options) { }
+
+
+
+    }
+}
